6e9ffdbd6f5ace9055f3072f54ca5235f7fb3523
Music-Wandee <Wandee.cpe@gmail.com>
2020-01-14 22:46:50 +0700

    Upload some changes for day2

--- word2vec/word2vec.py
+++ word2vec/word2vec.py
@@ -1,67 +1,56 @@
 from gensim.models import Word2Vec
-from openpyxl import Workbook
 
-def write_ver_excel(model):
-    # write report (Vertical)
-    vertical_similarity = list()
-    words = list(model.wv.vocab)
-    for i in range(len(words)):
-        for j in range(len(words)):
-            vec = model.wv.similarity(words[i],words[j])
-            vertical_similarity.append([words[i], words[j], vec])   
- 
-    # write excel
-    wb = Workbook(write_only=True)
-    ws = wb.create_sheet()
-    for idx in range(len(vertical_similarity)):
-        ws.append(vertical_similarity[idx])
-    wb.save('word_similarity.xlsx')
+def get_word_frequency(model):
     
-def write_matrix_excel(model):
-    # write report (Matrix) 
-    co_sin_list = list()
-    words = list(model.wv.vocab)
-    for i in range(len(words)):
-        tmp = list()
-        for j in range(len(words)):
-            vec = model.wv.similarity(words[i],words[j])
-            tmp.append(vec)       
-        co_sin_list.append(tmp)
+    word_frequency = list()
+    vocab = list(model.wv.vocab)
     
-    # write report (Matrix)      
-    wb = Workbook(write_only=True)
-    ws = wb.create_sheet()
-    words.insert(0,' ')
-    ws.append(words)
-    del words[0]
-    for idx in range(len(co_sin_list)):
-        tmp_lst = list()
-        tmp_lst = co_sin_list[idx]
-        tmp_lst.insert(0, words[idx])
-        ws.append(tmp_lst)
-    wb.save('word_similarity_matrix.xlsx')
+    for word in vocab:
+        word_info = model.wv.vocab[word]
+        word_frequency.append(word + '	' + str(word_info.count))
+        
+    return word_frequency
 
 
-def word2vec(tokenized_data):
+def write_text(file_name, data):
+    with open(file_name, 'w', encoding='utf8') as file:
+        for line in data:
+            file.write(line)
+            file.write('\n')
     
-    # Word2Vec parameter
-    # Reference https://radimrehurek.com/gensim/models/word2vec.html
-    # size: Dimensionality of the word vectors.
-    # window: Maximum distance between the current and predicted word within a sentence.
-    # min_count: Ignores all words with total frequency lower than this.
-    
-    model = Word2Vec(tokenized_data, size=100, window=5, min_count=5)
-    model.save("word2vec.model")
-    #model = Word2Vec.load("word2vec.model")
-    
-    # Get vocabulary
-    words = list(model.wv.vocab)
-    
-    # Get word similarity
-    word_sim = model.wv.similarity('เลี้ยงลูก','มีความสุข')
-    print(word_sim)
-    
-    # Get most word similarity
-    most_sim = model.wv.most_similar('ลาออก')
-    print(most_sim)
-  
+# Word2Vec parameter
+# Reference https://radimrehurek.com/gensim/models/word2vec.html
+# size: Dimensionality of the word vectors.
+# window: Maximum distance between the current and predicted word within a sentence.
+# min_count: Ignores all words with total frequency lower than this.
+
+model = Word2Vec(tokenized_data, size=100, window=5, min_count=5)
+model.save("word2vec.model")
+#model = Word2Vec.load("word2vec.model")
+
+# Get vocabulary
+words = list(model.wv.vocab)
+
+# Get word similarity
+word_sim = model.wv.similarity('เลี้ยงลูก','มีความสุข')
+print(word_sim)
+
+# Get most word similarity
+most_sim = model.wv.most_similar('ลาออก')
+print(most_sim)
+
+
+'''
+# Get word frequency
+word_frequency = get_word_frequency(model)
+
+# write word similarity
+write_matrix_excel(model)
+write_ver_excel(model)
+
+# Write text file
+write_text('word_frequency.txt', word_frequency)
+
+# Visualization
+# https://www.vosviewer.com/
+'''
